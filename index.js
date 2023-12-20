b8e9bebddd5e0956a8c34102bc5af4c646562656
shaid30044 <kaona30044@gmail.com>
2023-12-20 22:27:24 +0600

    implement get api for user by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(
@@ -36,6 +36,14 @@ async function run() {
     app.get("/mobiles", async (req, res) => {
       const cursor = mobileCollection.find();
       const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    app.get("/mobiles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mobileCollection.findOne(query);
 
       res.send(result);
     });
